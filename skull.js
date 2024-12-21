776a95c64b961bb5c1f1d4cbe0ccc49c3ac461d5
Ben Floyd <ben.floyd@simplot.com>
2024-12-21 16:49:12 -0700

    Fix bug on level set after restart

--- skull.js
+++ skull.js
@@ -1,7 +1,7 @@
 class FlappyBird {
     constructor() {
         // Add starting level configuration
-        this.startingLevel = 1; // Can be modified for testing different levels
+        this.startingLevel = 10; // Can be modified for testing different levels
         this.speedIncreasePerLevel = 0.5; // 50% increase per level, can be modified
         this.pillarSpaceIncreasePerLevel = .05; // Increase pillar spacing per level
         
@@ -216,11 +216,11 @@ class FlappyBird {
         
         this.levelComplete = false;
         
-        // Reset speed to base speed
-        this.currentSpeed = this.baseSpeed;
-        
-        // Reset to starting level instead of level 1
+        // Reset to starting level first
         this.currentLevel = this.startingLevel;
+        
+        // Then set speed based on the starting level
+        this.currentSpeed = this.baseSpeed * (1 + (this.startingLevel - 1) * this.speedIncreasePerLevel);
         
         this.background.torches.forEach(torch => {
             torch.flameOffset = Math.random() * Math.PI;
